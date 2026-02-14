5ef01078a2ffffec2e1c452ca7ecbdb443b31db7
user <user@Sam-2.local>
2026-02-14 17:28:54 +0500

    added peerings

---
+++ Multi-cloud_Infrastucture/peerings.tf
@@ -0,0 +1,27 @@
+resource "azurerm_virtual_network_peering" "indiac-indiag" {
+  name                      = "indiawindowsclient-indiagateway"
+  resource_group_name       = azurerm_resource_group.windows-client.name
+  virtual_network_name      = azurerm_virtual_network.windows-client.name
+  remote_virtual_network_id = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_virtual_network_peering" "indiag-indiac" {
+  name                      = "indiagateway-indiawindowsclient"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example.name
+  remote_virtual_network_id = azurerm_virtual_network.windows-client.id
+}
+
+resource "azurerm_virtual_network_peering" "norway-india" {
+  name                      = "norway-india"
+  resource_group_name       = azurerm_resource_group.windows-ad.name
+  virtual_network_name      = azurerm_virtual_network.windows-ad.name
+  remote_virtual_network_id = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_virtual_network_peering" "india-norway" {
+  name                      = "india-norway"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example.name
+  remote_virtual_network_id = azurerm_virtual_network.windows-ad.id
+}
